ca403a649642b63ef03e0c92a78fefe448e9185c
Dequisitor <enantiodromy@gmail.com>
2015-08-05 20:55:41 +0200

    - create new iteration and moved to dedicated branch - removed unused styl file for dprocessor - changed legend UI - changed chart logic - y-axis placement is still not working

--- public/Scripts/Controllers/dprocessor.js
+++ public/Scripts/Controllers/dprocessor.js
@@ -2,102 +2,155 @@ var dprocess = angular.module("dprocess", ["mgcrea.ngStrap", "sharedServices"]);
 
 dprocess.controller("dprocessController", function ($scope, listService, $http, $alert) {
 	
+	$scope.chart = null;
+	$scope.data = null;
+	$scope.yAxis = null;
+
 	//load entries
 	listService.getLatestEntries(function (files, entries) {
 		$scope.availableFiles = files;
 	});
 
-	$scope.getYAxis = function (list, unit) {
+	$scope.getYAxis = function (unit) {
 		var current = -1;
-		for (var i=0; i<list.length; i++) {
-			if (list[i].title == unit.toLowerCase()) {
+		for (var i=0; i<$scope.yAxis.length; i++) {
+			if ($scope.yAxis[i].title == unit.toLowerCase()) {
 				current = i;
 			}
 		};
 
 		if (current == -1) { //add new unit to a new y axis
-			list.push({
-               title: unit,
-               labels: {
-                        format: "{value} " + unit
-                },
-				opposite: list.length%2==1
+			$scope.yAxis.push({ 
+				title: unit
 			});
-			return list.length-1;
+			return $scope.yAxis.length-1;
 		};
 
 		return current;
 	};
 
-	$scope.getDataSlot = function (data, entry, yAxis) {
+	$scope.filterAxis = function() {
+		var result = [];
+
+		$scope.yAxis.forEach(function (axis) {
+			result.push({
+				title: axis.title,
+				labels: {
+						format: "{value} " + axis.title
+				},
+				opposite: result.length%2==1
+			});
+		});
+
+		return result;
+	}
+
+	$scope.getDataSlot = function (entry) {
 		var current = -1;
-		for (var j=0; j<data.length; j++) {
-			if (data[j].name == entry.name) {
+		for (var j=0; j<$scope.data.length; j++) {
+			if ($scope.data[j].name == entry.name) {
 				current = j;
 			}
 		}
 		if (current == -1) { //add if it is new entry
-			var axis = $scope.getYAxis(yAxis, entry.unit);
-			data.push({
-                type: "spline",
+			var axis = $scope.getYAxis(entry.unit); //just to fill the yAxis array
+			$scope.data.push({
 				name: entry.name, 
-				yAxis: axis,
+				visible: true,
+				yAxis: entry.unit,
+				data: [],
+				type: "spline",
+				color: Highcharts.getOptions().colors[$scope.data.length],
 				tooltip: {
 					valueSuffix: entry.unit
-				},
-				data: []});
-			current = data.length -1;
+				}
+			});
+			current = $scope.data.length -1;
 		}
 
 		return current;
 	};
 
-	$scope.createChart = function (data, yAxis, fileName) {
-        angular.element("#chartContainer").highcharts({
-            chart: {
-                zoomType: "x",
-                plotBorderWidth: 1
-            },
-            title: {
-                text: fileName
-            },
-            tooltip: {
-                shared: true,
-                crosshairs: true,
-                valueDecimals: 2
-            },
-            xAxis: {
-                type: "datetime",
-                minRange: 24*3600*1000,
-                title: {
-                    text: "Time"
-                },
-                tickInterval: 7*24*3600*1000,
-                gridLineWidth: 1
-            },
-            yAxis: yAxis,
-            series: data
-        });		
+	$scope.filterData = function () {
+		var result = [];
+
+		$scope.data.forEach(function (data) {
+			if (data.visible) {
+				result.push({
+					name: data.name, 
+					yAxis: $scope.getYAxis(data.yAxis),
+					data: data.data,
+					type: data.type,
+					color: data.color,
+					fillOpacity: 0.3,
+					tooltip: data.tooltip
+				});
+			};
+		});
+
+		return result;
+	};
+
+	$scope.toggleSeries = function () {
+
+	};
+
+	$scope.createChart = function (fileName) {
+		//if (!$scope.chart) {
+			angular.element("#chartContainer").highcharts({
+				chart: {
+					zoomType: "x",
+					plotBorderWidth: 1
+				},
+				legend: {
+					enabled: false
+				},
+				title: {
+					text: fileName
+				},
+				tooltip: {
+					shared: true,
+					crosshairs: true,
+					valueDecimals: 2
+				},
+				xAxis: {
+					type: "datetime",
+					minRange: 24*3600*1000,
+					title: {
+						text: "Time"
+					},
+					tickInterval: 7*24*3600*1000,
+					gridLineWidth: 1
+				},
+				yAxis: $scope.filterAxis(),
+				series: $scope.filterData()
+			});
+			//$scope.chart = angular.element("#chartContainer").highcharts();
+		//} else {
+			//$scope.chart.series.data = $scope.filterData();
+			//$scope.chart.yAxis = $scope.filterAxis();
+			//$scope.chart.redraw();
+		//}
 	};
 
 	$scope.drawChart = function (fileName) {
 		$http.get("../../Data/" + fileName).success(function(json) {
-			var data = [];
-			var yAxis = [];
+			$scope.data = [];
+			$scope.yAxis = [];
 			json.forEach(function (entry) {
 				for (var i=0; i<entry.data.length; i++) {
 					if (!!entry.data[i].unit) {
 						//search for data slot
-						var current = $scope.getDataSlot(data, entry.data[i], yAxis);
+						var current = $scope.getDataSlot(entry.data[i]);
 
 						//add data
 						var date = new Date(Date.parse(entry.timeStamp.replace(/Z/g,"")));
 						var timeStamp = Date.UTC(date.getFullYear(), date.getMonth(), date.getDate());
-						data[current].data.push([timeStamp, entry.data[i].value]);
+						$scope.data[current].data.push([timeStamp, entry.data[i].value]);
 					}
 				}
 			});
-			$scope.createChart(data, yAxis, fileName);
+			$scope.createChart(fileName);
 		});
 	};
 
