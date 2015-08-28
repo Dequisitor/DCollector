ff34dcce57fd69059e953fafac989481364eb8f0
Dequisitor <enantiodromy@gmail.com>
2015-08-28 14:00:27 +0000

    - changed chart behaviour - multiple singe-series charts with synchronized crosshairs - navbar is no longer fixed on top (to create more space on mobile   devices)

--- public/scripts/controllers/dprocessor.js
+++ public/scripts/controllers/dprocessor.js
@@ -4,38 +4,136 @@ dprocess.controller("dprocessController", function ($scope, listService, $http, 
 	
 	$scope.chart = null;
 	$scope.data = null;
+	$scope.avgData = null;
 
 	//load entries
 	listService.getLatestEntries(function (files, entries) {
 		$scope.availableFiles = files;
 	});
 
-	$scope.getYAxis = function (data) {
+	$scope.filterData = function (input) {
 		var result = [];
 
-		for (var i=0; i<data.length; i++) {
-			var current = -1;
-			for (var j=0; j<result.length; j++) {
-				if (result[j].title == data[i].yAxis) {
-					current = j;
-				}
-			}
-
-			if (current == -1) { //create new axis
+		input.forEach(function (data) {
+			if (data.visible) {
 				result.push({
-					title: data[i].yAxis,
-					opposite: result.length%2==1,
-					labels: {
-						format: "{value} " + data[i].yAxis
-					}
+					name: data.name, 
+					yAxis: data.yAxis,
+					data: data.data,
+					type: data.type,
+					color: data.color,
+					fillOpacity: 0.3,
+					tooltip: data.tooltip,
+					dashStyle: data.dashStyle ? data.dashStyle : "Solid",
+					negativeColor: data.negativeColor ? data.negativeColor : null,
+					lineWidth: data.lineWidth ? data.lineWidth : 2
 				});
-				current = result.length -1;
-			}
-
-			data[i].yAxis = current;
-		}
+			};
+		});
 
 		return result;
+	};
+
+	$scope.createChart = function () {
+		//var data = $scope.filterData($scope.data);
+		var data = $scope.data;
+
+		//destroy previous charts
+		for (var i=Highcharts.charts.length-1; i>=0; i--) {
+			if (!Highcharts.charts[i]) {
+				continue;
+			}
+			Highcharts.charts[i].destroy();
+		}
+		angular.element("#chartContainer").empty();
+
+		//create new charts
+		data.forEach(function (entry) {
+			var chart = angular.element("<div class='chart'></div>");
+			angular.element("#chartContainer").append(chart);
+			chart.highcharts({
+				chart: {
+					//zoomType: "x",
+				},
+				legend: {
+					enabled: false
+				},
+				credits: {
+					enabled: false
+				},
+				title: {
+					text: entry.name + "[" + entry.tooltip.valueSuffix + "]",
+					align: "left",
+					style: {
+						fontSize: '14px'
+					}
+				},
+				plotOptions: {
+					series: {
+						allowPointSelect: true,
+						marker: {
+							enabled: false
+						},
+						states: {
+							hover: {
+								lineWidthPlus: 0
+							}
+						},
+						point: {
+							events: {
+								mouseOver: function (e) {
+									for (var i=0; i<Highcharts.charts.length; i++) {
+										var chart = Highcharts.charts[i];
+										if (!chart) {
+											continue;
+										}
+
+										var series = chart.series[0];
+										for (var j=0; j<series.data.length && series.data[j].x < this.x; j++) {
+										}
+
+										if (j < series.data.length) {
+											chart.tooltip.refresh(series.points[j]);
+											chart.xAxis[0].removePlotLine('crosshair');
+											chart.xAxis[0].addPlotLine({
+												id: 'crosshair',
+												color: 'rgba(100, 100, 100, 0.8)',
+												width: 1,
+												value: series.points[j].x,
+												zIndex: 3
+											});
+											series.data[j].select();
+										}
+									}
+								}
+							}
+						}
+					}
+				},
+				tooltip: {
+					//crosshairs: true,
+					valueDecimals: 2,
+					positioner: function () {
+						return { x: this.chart.chartWidth - this.label.width, y: -1 };
+					},
+					shadow: false,
+					borderWidth: 0,
+					backgroundColor: 'rgba(255, 255, 255, 0.0)'
+				},
+				xAxis: {
+					type: "datetime",
+					minRange: 24*3600*1000,
+					tickInterval: 7*24*3600*1000,
+					gridLineWidth: 1
+				},
+				yAxis: [{
+					title: {
+						text: null,
+					}
+				}],
+				series: [entry]
+			});
+		});
 	};
 
 	$scope.getDataSlot = function (entry) {
@@ -48,78 +146,23 @@ dprocess.controller("dprocessController", function ($scope, listService, $http, 
 		if (current == -1) { //add if it is new entry
 			$scope.data.push({
 				name: entry.name, 
-				visible: true,
-				yAxis: entry.unit,
+				visible: true, 
 				data: [],
 				type: "spline",
+				lineWidth: 1,
 				color: Highcharts.getOptions().colors[$scope.data.length],
 				tooltip: {
 					valueSuffix: entry.unit
-				}
-			});
+				}			});
 			current = $scope.data.length -1;
 		}
 
 		return current;
 	};
 
-	$scope.filterData = function () {
-		var result = [];
-
-		$scope.data.forEach(function (data) {
-			if (data.visible) {
-				result.push({
-					name: data.name, 
-					yAxis: data.yAxis,
-					data: data.data,
-					type: data.type,
-					color: data.color,
-					fillOpacity: 0.3,
-					tooltip: data.tooltip
-				});
-			};
-		});
-
-		return result;
-	};
-
-	$scope.createChart = function () {
-		var data = $scope.filterData();
-		var yAxis = $scope.getYAxis(data);
-		angular.element("#chartContainer").highcharts({
-			chart: {
-				zoomType: "x",
-				plotBorderWidth: 1
-			},
-			legend: {
-				enabled: false
-			},
-			title: {
-				text: $scope.selectedFile
-			},
-			tooltip: {
-				shared: true,
-				crosshairs: true,
-				valueDecimals: 2
-			},
-			xAxis: {
-				type: "datetime",
-				minRange: 24*3600*1000,
-				title: {
-					text: "Time"
-				},
-				tickInterval: 7*24*3600*1000,
-				gridLineWidth: 1
-			},
-			yAxis: yAxis,
-			series: data
-		});
-	};
-
 	$scope.drawChart = function () {
 		$http.get("/dprocessor/data/" + $scope.selectedFile).success(function(json) {
 			$scope.data = [];
-			$scope.yAxis = [];
 			json.forEach(function (entry) {
 				for (var i=0; i<entry.data.length; i++) {
 					if (!!entry.data[i].unit) {
@@ -133,6 +176,7 @@ dprocess.controller("dprocessController", function ($scope, listService, $http, 
 					}
 				}
 			});
+
 			$scope.createChart($scope.selectedFile);
 		});
 	};
@@ -142,6 +186,8 @@ dprocess.controller("dprocessController", function ($scope, listService, $http, 
 	};
 
 	$scope.redrawChart = function(pos, type) {
+		var chart = angular.element("#chartContainer").highcharts();
+
 		var index = pos;
 		for (var i=0; i<pos; i++) {
 			if (!$scope.data[i].visible) {
@@ -149,10 +195,22 @@ dprocess.controller("dprocessController", function ($scope, listService, $http, 
 			}
 		}
 
-		var chart = angular.element("#chartContainer").highcharts();
 		chart.series[index].update({
 			type: type
 		});
 	};
 
+//	$scope.synchronizeChartPosition = function (e) {
+//		for (var i=0; i<Highcharts.charts.length; i++) {
+//			var chart = Highcharts.charts[i];
+//			e = chart.pointer.normalize(e);
+//			var point = chart.series[0].searchPoint(e, true);
+//
+//			if (point) {
+//				point.onMouseOver();
+//				chart.tooltip.refresh(point);
+//				chart.xAxis[0].drawCrosshair(e, point);
+//			}
+//		}
+//	}
 });
